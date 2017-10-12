1404795bbe76e51cf6df2eb826d32255d0ed2bc9
Victor Fan <vsfan@google.com>
2017-10-12 13:51:51 -0700

    Revised xlogs, server detection, bug fixes

--- eureka.rb
+++ eureka.rb
@@ -4,12 +4,13 @@ require 'uri'
 require 'json'
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
 
 $last_charname = nil
-$last_server = nil
+$last_servers = nil
 
 ZONES = {
-	4 => [1034, 1035],					# Sophia and Zurvan
+	4 => [1034, 1035, 1038],			# Stormblood Primals
 	6 => [17],							# A4S
 	9 => [25],							# A8S
 	13 => [34, 35, 36, 37],				# A9-A12S
@@ -18,7 +19,7 @@ ZONES = {
 }
 
 ENCOUNTERS = {
-	42 => "O1S", 43 => "O2S", 44 => "O3S", 45 => "Exfaust", 46 => "O4S", 1036 => "Partydad", 1037 => "Snakemom",
+	42 => "O1S", 43 => "O2S", 44 => "O3S", 45 => "Exfaust", 46 => "O4S", 1036 => "Partydad", 1037 => "Snakemom", 1038 => "Dragondad",
 	34 => "A9S", 35 => "A10S", 36 => "A11S", 37 => "A12S", 25 => "A8S", 17 => "A4S", 1034 => "Sophia", 1035 => "Zurvan"
 }
 
@@ -84,8 +85,9 @@ bot.command :ls do |event, *args|
 end
 
 bot.command :xlogs do |event, *args|
-	event << "Quick lookup for *#{$last_charname}* (#{$last_server}):"
-	respondLogs(event, [15, 17], $last_charname.split + [$last_server])
+	i = (args[0]) ? (args[0].to_i - 1) : 0
+	event << "Quick lookup for *#{$last_charname}* (#{$last_servers[i]}):"
+	respondLogs(event, [15, 17], $last_charname.split + [$last_servers[i]])
 end
 
 bot.command :math do |event, *args|
@@ -99,13 +101,14 @@ end
 
 bot.command :help do |event, *args|
 	p event.server.id
-	event << "**!logs** *charactername* *server*: gets data for all current patch content."
-	event << "**!esports** *charactername* *server*: gets data for all current raid content."
-	event << "**!bsports** *charactername* *server*: gets data for previous patch raid content."
-	event << "**!zsports** *charactername* *server*: gets data for selected encounters in previous expansions."
-	event << "**!ls** *charactername* *datacenter*: look up what server that 11 dps scholar in your PF could be from. case-sensitive. fuck sqex."
+	event << "**!logs** *name* *server*: gets data for all current patch content."
+	event << "**!esports** *name* *server*: gets data for all current raid content."
+	event << "**!bsports** *name* *server*: gets data for previous patch raid content."
+	event << "**!zsports** *name* *server*: gets data for selected encounters in previous expansions."
+	event << "**!ls** *name* *dc*: find charcters in a datacenter with a name, and their 70 classes."
 	event << "**!xlogs**: shortcut to make a !logs request with the top result of the last !ls request"
-	event << "server can be omitted and will default to excal; datacenter can be omitted and will default to primal."
+	event << "**!xlogs** *n*: shortcut to make a !logs request with the Nth result of the last !ls request, counting from 1."
+	event << "*server* defaults to excalibur. *dc* defaults to primal. xlogs won't work for JP servers."
 end
 
 def respondLogs(event, zones, args)
@@ -121,6 +124,11 @@ def respondLogs(event, zones, args)
 		event << "FFlogs request errored; charactername/server are probably wrong"
 		raw = {}
 		scrub = false
+	end
+
+	if raw["hidden"]
+		event << "User manually disabled their FFlogs. They're probably garbage and sensitive about it."
+		return
 	end
 
 	raw.each do |cid, data|
@@ -150,7 +158,7 @@ def respondLodestone(event, name, datacenter)
 
 	if data.length > 0
 		$last_charname = name
-		$last_server = data.first.first
+		$last_servers = data.keys
 		event << "Results for *#{name}* on #{datacenter}:"
 	else
 		event << "No results found..."
@@ -170,7 +178,8 @@ end
 # GET and JSON-encode a single FFlogs metrics request for a character + fight combination
 def fflogs_fight(name, server, zone, encounter)
 	# https://www.fflogs.com:443/v1/parses/character/Mistel%20Aventice/Excalibur/NA?zone=17&encounter=42&api_key=d111a2f951c186357796eca246ca2640
-	uri = URI.parse(URI::encode("https://www.fflogs.com:443/v1/parses/character/" + name + "/" + server + "/NA"))
+	region = getRegion(server)
+	uri = URI.parse(URI::encode("https://www.fflogs.com:443/v1/parses/character/" + name + "/" + server + "/" + region))
 	params = {:api_key => "d111a2f951c186357796eca246ca2640", :zone => zone, :encounter => encounter}
 	uri.query = URI.encode_www_form( params )
 	p "GET " + uri.to_s
@@ -188,6 +197,7 @@ def analyze(name, query_zones, server)
 		next unless query_zones.include?(zone)
 		encounters.each do |eid|
 			response = fflogs_fight(name, server, zone, eid)
+			return response if response["hidden"]
 			next if response.count == 0
 			specs = response.first["specs"]
 			specs.each do |spec|
@@ -242,6 +252,25 @@ end
 def displayClass(event, classname)
 	serverid = event.server.id
 	return EMOJI[classname]
+end
+
+AETHER = ['adamantoise', 'balmung', 'cactuar', 'coeurl', 'faerie', 'gilgamesh', 'goblin', 'jenova', 'mateus', 'midgardsormr', 'sargatanas', 'siren', 'zalera']
+PRIMAL = ['behemoth', 'brynhildr', 'diabolos', 'excalibur', 'exodus', 'famfrit', 'hyperion', 'lamia', 'leviathan', 'malboro', 'ultros']
+CHAOS = ['cerberus', 'lich', 'louisoix', 'moogle', 'odin', 'omega', 'phoenix', 'ragnarok', 'shiva', 'zodiark']
+ELEMENTAL = ['aegis', 'atomos', 'carbuncle', 'garuda', 'gungnir', 'kujata', 'ramuh', 'tonberry', 'typhon', 'unicorn']
+GAIA = ['alexander', 'bahamut', 'durandal', 'fenrir', 'ifrit', 'ridill', 'tiamat', 'ultima', 'valefor', 'yojimbo', 'zeromus']
+MANA = ['anima', 'asura', 'belias', 'chocobo', 'hades', 'ixion', 'mandragora', 'masamune', 'pandemonium', 'shinryu', 'titan']
+def getRegion(server)
+	server = server.downcase
+	if (AETHER + PRIMAL).include?(server)
+		return 'NA'
+	elsif CHAOS.include?(server)
+		return 'EU'
+	elsif (ELEMENTAL + GAIA + MANA).include?(server)
+		return 'JP'
+	else
+		return nil
+	end
 end
 
 bot.run
